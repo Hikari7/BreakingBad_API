1311deffb43b349eefb26a84f2dbccc4b7a6d441
Hikari7 <h.kobe712@gmail.com>
2022-07-20 11:27:25 -0700

    added

--- main.js
+++ main.js
@@ -13,27 +13,35 @@ async function fetchinfo() {
   //characters全部を出力するならforEach使えばいいと思う
   lists.textContent = "";
   chars.forEach(function (char) {
-    const pic = document.createElement('img');
-    pic.classList.add("pic");
-    const h2 = document.createElement("h2");
-    h2.textContent = "Nickname";
-    h2.classList.add("h2");
+    const pic = document.createElement("img");
+    pic.classList.add("char_pic");
+    pic.classList.remove("hidden");
+
+    // const h2 = document.createElement("h2");
+    // h2.textContent = "Nickname";
+    // h2.classList.add("h2");
+
     const name = document.createElement("li");
     const nickname = document.createElement("li");
-    h2.classList.add("h2");
+    const birthday = document.createElement("li");
+    const occupation = document.createElement("li");
+    // h2.classList.add("h2");
     name.classList.add("li");
     nickname.classList.add("li");
-    h2.textContent = "Nickname";
+    birthday.classList.add("li");
+    occupation.classList.add("li");
+    // h2.textContent = "Nickname";
     name.textContent = char.name;
     nickname.textContent = char.nickname;
+    birthday.textContent = char.birthday;
+    occupation.textContent = char.occupation;
     pic.setAttribute("src", `${char.img}`);
-    // pic.src = char."https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg";
-    //  let catsImgEl = document.createElement("img"); //img要素作成
-    // catsImgEl.setAttribute("src", `${catsImgUrl}`);
 
+    // lists.appendChild(h2);
     lists.appendChild(name);
-    lists.appendChild(h2);
     lists.appendChild(nickname);
     lists.appendChild(pic);
+    lists.appendChild(birthday);
+    lists.appendChild(occupation);
   });
 }
