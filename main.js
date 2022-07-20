ace48c1578a7b3e3c86890692042f4d6716cc388
Hikari7 <h.kobe712@gmail.com>
2022-07-19 22:54:39 -0700

    added some scss

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const getInfo = document.querySelector(".get_info");
 const lists = document.getElementById("lists");
 const endpoint = "https://www.breakingbadapi.com/api/character/random";
+
+//btn for get randam character
 getInfo.addEventListener("click", fetchinfo); //bitchボタン押したらfetchinfo関数が呼ばれる
 async function fetchinfo() {
   //   Response オブジェクトから JSON の本文の内容を抽出するには、 json() メソッドを使用
@@ -11,20 +13,21 @@ async function fetchinfo() {
   //characters全部を出力するならforEach使えばいいと思う
   lists.textContent = "";
   chars.forEach(function (char) {
+    const pic = document.createElement('img');
+    pic.classList.add("pic");
     const h2 = document.createElement("h2");
     h2.textContent = "Nickname";
     h2.classList.add("h2");
     const name = document.createElement("li");
     const nickname = document.createElement("li");
-    const pic = document.createElement('img');
     h2.classList.add("h2");
     name.classList.add("li");
     nickname.classList.add("li");
     h2.textContent = "Nickname";
     name.textContent = char.name;
     nickname.textContent = char.nickname;
-    // pic.src = char."https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg";
     pic.setAttribute("src", `${char.img}`);
+    // pic.src = char."https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg";
     //  let catsImgEl = document.createElement("img"); //img要素作成
     // catsImgEl.setAttribute("src", `${catsImgUrl}`);
 
