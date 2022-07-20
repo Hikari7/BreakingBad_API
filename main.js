ca1ee22c44913b6c175605eba7145161137aabc1
Hikari7 <h.kobe712@gmail.com>
2022-07-20 16:57:08 -0700

    add DOMs

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const getInfo = document.querySelector(".get_info");
-const lists = document.getElementById("lists");
+const lists = document.querySelector(".lists");
 const endpoint = "https://www.breakingbadapi.com/api/character/random";
-
+const fetched_info = document.querySelector(".fetched_info");
+const char_pic = document.querySelector(".char_pic")
 //btn for get randam character
-getInfo.addEventListener("click", fetchinfo); //bitchボタン押したらfetchinfo関数が呼ばれる
-async function fetchinfo() {
+getInfo.addEventListener("click", () => getinfo()); //bitchボタン押したらfetchinfo関数が呼ばれる
+
+async function getinfo() {
   //   Response オブジェクトから JSON の本文の内容を抽出するには、 json() メソッドを使用
   const res = await fetch(endpoint);
   const chars = await res.json();
   console.log(chars);
+
   //DOM操作
   //characters全部を出力するならforEach使えばいいと思う
   lists.textContent = "";
+  fetched_info.textContent = "";
   chars.forEach(function (char) {
     const pic = document.createElement("img");
     pic.classList.add("char_pic");
-    pic.classList.remove("hidden");
 
     // const h2 = document.createElement("h2");
     // h2.textContent = "Nickname";
@@ -26,22 +29,25 @@ async function fetchinfo() {
     const birthday = document.createElement("li");
     const occupation = document.createElement("li");
     // h2.classList.add("h2");
-    name.classList.add("li");
-    nickname.classList.add("li");
-    birthday.classList.add("li");
-    occupation.classList.add("li");
+    name.classList.add("list");
+    nickname.classList.add("list");
+    birthday.classList.add("list");
+    occupation.classList.add("list");
     // h2.textContent = "Nickname";
     name.textContent = char.name;
     nickname.textContent = char.nickname;
     birthday.textContent = char.birthday;
     occupation.textContent = char.occupation;
     pic.setAttribute("src", `${char.img}`);
+    // console.log(pic);
+    // console.log(nickname);
 
     // lists.appendChild(h2);
     lists.appendChild(name);
     lists.appendChild(nickname);
-    lists.appendChild(pic);
     lists.appendChild(birthday);
     lists.appendChild(occupation);
+    console.log(name);
+    fetched_info.appendChild(pic);
   });
 }
